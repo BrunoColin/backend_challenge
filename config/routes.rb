0f58caf9c82c91f9690c24a72e08d57c9498e5bf
Bruno Colin <brunocolinxbox@hotmail.com>
2022-02-25 21:05:43 -0300

    login in/logout Admin

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
+  # devise_for :installs
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  namespace :admin do
+    root to: "home#index"
+  end
+
+end
---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+  it "admin is valid" do
+    admin = Admin.new(email: "bruno@bruno.com", password: "123456")
+
+    expect(admin.email). to eql "bruno@bruno.com"
+    expect(admin). to be_valid
+  end
+
+  it "admin is not valid" do
+    admin = Admin.new(email: "bruno@bruno.com")
+
+    expect(admin). to_not be_valid
+  end
+
+end
