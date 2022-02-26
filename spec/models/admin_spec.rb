e779bc79a3392e7afdcf7d4e041aa1c921c78dc3
Bruno Colin <brunocolinxbox@hotmail.com>
2022-02-25 22:23:51 -0300

    CRUD Admins

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   it "admin is valid" do
-    admin = Admin.new(email: "bruno@bruno.com", password: "123456")
+    admin = Admin.new(name: "Bruno", email: "bruno@bruno.com", password: "123456")
 
     expect(admin.email). to eql "bruno@bruno.com"
     expect(admin). to be_valid
